ae1f5f44aa16d681b9214908ee040002a431066a
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-07-27 14:57:04 -0300

    [Pedro] Atualizandopubspec.yaml e colocando colocando imagem de logo

--- lib/app/view/home_view.dart
+++ lib/app/view/home_view.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Homeview extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: [
-          Image.asset('assets/images/logo.png')
-        ],
+        children: [Image.asset('assets/logo.png')],
       ),
     );
   }
