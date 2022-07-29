0e9e576e02e491ffa6c695b93baee18b63ce99cc
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-07-29 12:30:58 -0300

    [Pedro] Curso de Flutter #30 - [Conversor de Moedas] Desenhando a View

--- lib/app/view/home_view.dart
+++ lib/app/view/home_view.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 
 class Homeview extends StatelessWidget {
+  const Homeview({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [Image.asset('assets/logo.png')],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: ListView(
+          children: [
+            Image.asset('assets/logo.png'),
+            Row(
+              children: [
+                Expanded(
+                    child: DropdownButton<String>(
+                        items: [], onChanged: (value) {})),
+                Expanded(
+                    child: TextField(
+                ))
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('CONVERTER'),
+            )
+          ],
+        ),
       ),
     );
   }
